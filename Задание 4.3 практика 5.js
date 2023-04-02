9222482c7debedf1dc9b7d18f1a9b0562724e174
Zr413 <z0rin.13@yandex.ru>
2023-04-02 02:48:24 +0100

    Adding a Lamp Class and Control Methods

--- Задание 4.3 практика 5.js
+++ Задание 4.3 практика 5.js
@@ -23,16 +23,64 @@ class ElectricalAppliance {
     }
 }
 
+//Класс лампы
+class Lamp extends ElectricalAppliance {
+    constructor(name, power, brightness) {
+        super(name, power);
+        this.brightness = brightness;
+    }
+
+    // Метод уменьшения яркости
+    dim() {
+        if (this.plugged) {
+            if (this.brightness > 0) {
+                this.brightness -= 1;
+                console.log(`Яркость лампы "${this.name}" уменьшена на 1. Текущая яркость: ${this.brightness}`);
+            } else {
+                console.log(`Яркость лампы "${this.name}" уже на минимуме.`);
+            }
+        } else {
+            console.log(`Сначала нужно включить лампу "${this.name}" в розетку.`);
+        }
+    }
+
+    // Метод увеличения яркости
+    brighten() {
+        if (this.plugged) {
+            if (this.brightness < 10) {
+                this.brightness += 1;
+                console.log(`Яркость лампы "${this.name}" увеличена на 1. Текущая яркость: ${this.brightness}`);
+            } else {
+                console.log(`Яркость лампы "${this.name}" уже на максимуме.`);
+            }
+        } else {
+            console.log(`Сначала нужно включить лампу "${this.name}" в розетку.`);
+        }
+    }
+
+    // Метод выключатель
+    turnOn() {
+        if (this.plugged) {
+            this.isOn = true;
+            console.log(`Лампа "${this.name}" (${this.power} Вт) включена.`);
+        } else {
+            console.log(`Сначала нужно включить лампу "${this.name}" в розетку.`);
+        }
+    }
+}
+
+
+const myLamp = new Lamp('Настольная лампа', 40, 1);
 
 myLamp.plugIn();
 myLamp.brighten();
 myLamp.dim();
 
 
-myComputer.plugIn();
-myComputer.start();
-myComputer.login("user", "1234");
-myComputer.logout();
-myComputer.login("admin", "password");
-myComputer.logout();
-myComputer.shutdown();
+// myComputer.plugIn();
+// myComputer.start();
+// myComputer.login("user", "1234");
+// myComputer.logout();
+// myComputer.login("admin", "password");
+// myComputer.logout();
+// myComputer.shutdown();
